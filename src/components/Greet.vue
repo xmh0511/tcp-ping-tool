afb68478fb48976034841419a64f7220a831655a
xmh0511 <970252187@qq.com>
2023-04-23 20:23:12 +0800

    first commit

---
+++ src/components/Greet.vue
@@ -0,0 +1,269 @@
+<script setup>
+import { reactive, ref, onUnmounted, onMounted } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+import { emit, listen, once } from "@tauri-apps/api/event";
+
+import { SyncOutlined } from "@ant-design/icons-vue";
+import { message } from "ant-design-vue";
+
+const per_pool = reactive({
+  text: "",
+  time_out_threshold: 5000,
+  socks5_url: "",
+  use_proxy: false,
+  interval:3000
+});
+
+const loading_state = ref(false);
+
+let unlisten1;
+let unlisten2;
+let unlisten3;
+
+onMounted(async () => {
+  unlisten1 = await listen("reset", (event) => {
+    //console.log(event);
+    message.error(`${event.payload}`);
+    loading_state.value = false;
+    dataSource.list = [];
+  });
+
+  unlisten2 = await listen("complete", (event) => {
+    message.success("本轮测试结束");
+    loading_state.value = false;
+  });
+
+  unlisten3 = await listen("per-result", (event) => {
+    //console.log(event);
+    let json = JSON.parse(event.payload);
+    if (json.success) {
+      if (dataSource.list.length === 0) {
+        return;
+      }
+      let index = json.index;
+      if (
+        dataSource.list[index] === undefined ||
+        dataSource.list[index] === ""
+      ) {
+        return;
+      }
+      //console.log(dataSource.list[index]);
+      dataSource.list[index].latency.result = `${json.msg.latency} ms`;
+      dataSource.list[index].latency.count = json.msg.count;
+      dataSource.list[index].latency.success = true;
+      dataSource.list[index].is_pending = false;
+    } else {
+      if (dataSource.list.length === 0) {
+        return;
+      }
+      let index = json.index;
+      dataSource.list[index].latency.result = json.msg.error;
+      dataSource.list[index].latency.count = "NaN";
+      dataSource.list[index].latency.success = false;
+      dataSource.list[index].is_pending = false;
+    }
+  });
+});
+
+onUnmounted(() => {
+  //console.log("onUnmounted");
+  unlisten1();
+  unlisten2();
+  unlisten3();
+});
+
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//   greetMsg.value = await invoke("greet", { name: name.value });
+// }
+const test_speed = async () => {
+  //console.log(per_pool.text);
+  if (loading_state.value === true) {
+    emit("cancel-all");
+    return;
+  }
+  let text = per_pool.text.trim();
+  let text_n = text.replace(/\r/g, "");
+  if (text_n === "") {
+    message.error(`填写测试地址`);
+    return;
+  }
+  let arr = text_n.split("\n");
+  //console.log(arr);
+  dataSource.list = [];
+  for (let per of arr) {
+    dataSource.list.push({
+      per,
+      latency: {
+        result: "",
+        success: true,
+        count: 0,
+      },
+      is_pending: true,
+    });
+  }
+  //console.log("-------------------",dataSource.list);
+  if (arr.length !== 0) {
+    loading_state.value = true;
+    emit("test-pers", {
+      pers: arr,
+      time_out: per_pool.time_out_threshold,
+      use_proxy: per_pool.use_proxy,
+      socks5_url: per_pool.socks5_url,
+	  interval: per_pool.interval
+    }); //["222.186.139.4:9099"]
+  }
+};
+const cleanAll = ()=>{
+	console.log("loading_state.value", loading_state.value);
+	if(loading_state.value === false){
+		dataSource.list = [];
+	}
+}
+const dataSource = reactive({
+  list: [],
+});
+const columns = reactive([
+  {
+    title: "远端",
+    dataIndex: "per",
+    key: "per",
+  },
+  {
+    title: "状态",
+    dataIndex: "latency",
+    key: "latency",
+  },
+  {
+    title: "次数",
+    dataIndex: "count",
+    key: "count",
+  },
+]);
+</script>
+
+<template>
+  <div class="card">
+    <div class="form-content">
+      <a-form>
+        <a-form-item label="超时阈值" :label-col="{ span: 3 }">
+          <a-input
+            v-model:value="per_pool.time_out_threshold"
+            suffix="ms"
+          ></a-input>
+        </a-form-item>
+		<a-form-item label="间隔时间" :label-col="{ span: 3 }">
+          <a-input
+            v-model:value="per_pool.interval"
+            suffix="ms"
+          ></a-input>
+        </a-form-item>
+        <a-form-item label="Socks5" :label-col="{ span: 3 }">
+          <div class="group-in-line">
+            <a-input
+              :disabled="!per_pool.use_proxy"
+              v-model:value="per_pool.socks5_url"
+            ></a-input>
+            <a-checkbox
+              class="checkbox-item"
+              v-model:checked="per_pool.use_proxy"
+            ></a-checkbox>
+          </div>
+        </a-form-item>
+        <a-form-item label="Tcp测试池" name="name" :label-col="{ span: 3 }">
+          <a-textarea
+            class="per-pool-textarea"
+            v-model:value="per_pool.text"
+            placeholder="每个地址:端口号独占一行"
+          />
+        </a-form-item>
+      </a-form>
+      <div class="button-content">
+        <a-button style="margin-right: 10px;" type="primary" @click="test_speed">{{
+          loading_state === false ? "开始" : "结束"
+        }}</a-button>
+		<a-button :disabled="loading_state" type="primary" danger @click="cleanAll">清除</a-button>
+      </div>
+    </div>
+    <div style="height: 20px; width: 100%">
+      <a-badge
+        v-if="loading_state === true"
+        status="processing"
+        text="进行中"
+      />
+    </div>
+    <div class="table-content">
+      <a-table
+        :dataSource="dataSource.list"
+        :columns="columns"
+        :pagination="false"
+      >
+        <template #bodyCell="{ column, record }">
+          <template v-if="column.key === 'latency'">
+            <p
+              v-if="record.is_pending === false"
+              class="latency-text"
+              :class="record.latency.success ? 'green-text' : 'red-text'"
+            >
+              {{ record.latency.result }}
+            </p>
+            <SyncOutlined v-else spin></SyncOutlined>
+          </template>
+          <template v-if="column.key === 'count'">
+            <a-badge
+              v-if="record.latency.success"
+              :count="record.latency.count"
+              :number-style="{ backgroundColor: '#52c41a' }"
+            />
+            <a-badge
+			  v-else
+              count="NaN"
+              :number-style="{
+                backgroundColor: '#fff',
+                color: '#999',
+                boxShadow: '0 0 0 1px #d9d9d9 inset',
+              }"
+            />
+          </template>
+        </template>
+      </a-table>
+    </div>
+  </div>
+</template>
+<style scoped>
+.card {
+  width: 80%;
+  margin: 0 auto;
+}
+.per-pool-textarea {
+  height: 150px;
+  resize: none;
+}
+.table-content {
+  margin-top: 10px;
+  max-height: 228px;
+  overflow-y: auto;
+}
+.latency-text {
+  margin-bottom: 0px;
+}
+.green-text {
+  color: green;
+}
+.red-text {
+  color: red;
+}
+.form-content {
+  padding-top: 15px;
+}
+.button-content {
+  text-align: right;
+}
+.group-in-line {
+  display: flex;
+  align-items: center;
+}
+.group-in-line .checkbox-item {
+  margin-left: 10px;
+}
+</style>
